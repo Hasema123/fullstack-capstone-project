2ca62230e7f6d723bc57610e4a51b8b4bb5a4ec3
Hasema123 <dbauiwe@outlook.de>
2024-10-05 15:00:36 +0200

    Create the MainPage and Navbar

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -4,10 +4,15 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">GiftLink</a>
-
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
-                    {/* Task 1: Add links to Home and Gifts below*/}
+
+                    <li className="nav-item">
+                    <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/app">Gifts</a> {/* Updated Link */}
+                    </li>
                 </ul>
             </div>
         </nav>
