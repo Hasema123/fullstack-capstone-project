2133f0529257757105db304bff7376edcf543296
Hasema123 <dbauiwe@outlook.de>
2024-10-06 10:08:15 +0200

    Implement the Details Page

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,21 +3,23 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 function App() {
   const navigate = useNavigate();
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-        </Routes>
-        </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+      </Routes>
+    </>
   );
 }
 export default App;
